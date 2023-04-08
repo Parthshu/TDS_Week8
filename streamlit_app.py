1a9135b1da525ee453002d74d57f1651be3b2f22
Parth S Shah <96365171+Parthshu@users.noreply.github.com>
2023-04-08 15:17:53 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,15 +11,17 @@ This app gives largest number from given three input numbers
 st.header('User Input Parameters')
 
 def user_input_features():
+    
+    
     first_number = st.number_input("Enter the first number and press Enter",step=1)
     second_number = st.number_input("Enter the second number and press Enter",step=1)
     third_number = st.number_input("Enter the third number and press Enter",step=1)
-
-    data = [first_number,second_number,third_number]
+    
+    data = {'Numbers': first_number,second_number,third_number}
     return data
  
 df = user_input_features()
-max_number = max(data)
+max_number = max(data['Numbers'])
 
 st.header('Result')
 st.subheader(f'The Largest among the three numbers is {max_number}.')
